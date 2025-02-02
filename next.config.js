fbf6d7dfc19150db25501558115bacdec8e93322
Aman <54789531+eppisai@users.noreply.github.com>
2025-02-02 14:25:51 +0530

    next js

--- next.config.js
+++ next.config.js
@@ -36,6 +36,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+       protocol: "https",
+       hostname: "trulyurs-media.s3.ap-south-1.amazonaws.com"
+      }
     ],
   },
 }
